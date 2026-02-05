9df73820e6319d1716f62ca2fb135fcdd94187f7
Jatin <Jatin.Wakodikar@cognizant.com>
2026-02-05 16:47:06 +0530

    intialized

---
+++ src/main/java/org/biotrack/trialphasemgmt/TrialphasemgmtApplication.java
@@ -0,0 +1,13 @@
+package org.biotrack.trialphasemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialphasemgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialphasemgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/biotrack/trialphasemgmt/TrialphasemgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.biotrack.trialphasemgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialphasemgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
